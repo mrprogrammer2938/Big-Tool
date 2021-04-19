769af58a2756fc318cb78934cb53e56915ef4965
Ms.programmer <78996423+msprogrammer2938@users.noreply.github.com>
2021-04-19 03:53:10 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+# This code write by (Ms.nope)
+# installing
+if [[ "$(id -u)" -ne 0 ]]; then
+   echo ""
+   echo "Please, run this program as root!"
+   exit 1
+fi
+
+clear
+echo "installing..."
+sleep 1
+echo "
+    _______  ___   _______         _______  _______  _______  ___     
+   |  _    ||   | |       |       |       ||       ||       ||   |    
+   | |_|   ||   | |    ___| ____  |_     _||   _   ||   _   ||   |    
+   |       ||   | |   | __ |____|   |   |  |  | |  ||  | |  ||   |    
+   |  _   | |   | |   ||  |         |   |  |  |_|  ||  |_|  ||   |___ 
+   | |_|   ||   | |   |_| |         |   |  |       ||       ||       |
+   |_______||___| |_______|         |___|  |_______||_______||_______|
+           version 1.2.0
+"
+echo ""
+apt install python3
+apt install python
+apt install python3-pip
+pip install requirments.txt
+echo ""
+echo "finish!"
+echo ""
+echo "usage:
+       ./tools"
+exit 1
